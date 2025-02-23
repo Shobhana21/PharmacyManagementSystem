1313861fbb7e310c751f6cc6f1e63c54faa8fcbc
shobhana <shobhana21005@gmail.com>
2025-02-23 21:13:54 +0530

    implemented medicine class

--- src/model/Medicine.java
+++ src/model/Medicine.java
@@ -1,5 +1,26 @@
 package model;
 
 public class Medicine {
+	    private String medicineId;
+	    private String name;
+	    private double price;
+	    private int quantity;
+	    
+	    public Medicine(String medicineId, String name, double price, int quantity) {
+	        this.medicineId = medicineId;
+	        this.name = name;
+	        this.price = price;
+	        this.quantity = quantity;
+	    }
+	    
+	    public String getMedicineId() { return medicineId; }
+	    public String getName() { return name; }
+	    public double getPrice() { return price; }
+	    public int getQuantity() { return quantity; }
+	    
+	    public void setQuantity(int quantity) { this.quantity = quantity; }
 
+	    public String toString() {
+	        return "Medicine ID: " + medicineId + ", Name: " + name + ", Price: $" + price + ", Quantity: " + quantity;
+	    }
 }
